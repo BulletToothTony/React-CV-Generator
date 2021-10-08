960e119961b3105315c4bd8bfc5cb64be93f0e0b
Henry WL <henrywl@gmail.com>
2021-10-09 00:20:40 +0100

    Education styling

--- src/components/RenderEducation.js
+++ src/components/RenderEducation.js
@@ -3,14 +3,40 @@ import React, { useContext } from "react";
 import { AppContext } from "./context";
 
 const RenderEducation = () => {
-    const data = useContext(AppContext);
-    return (
-      <section className="EducationSect">
-        <h3 className="underlined">Education</h3>
-        <p>{data.educationForm.University}</p>
+  const data = useContext(AppContext);
+  return (
+    <section className="EducationSect">
+      <h3 className="underlined">Education</h3>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <div style={{ display: "flex" }}>
+          <div
+            style={{ width: "50%", fontWeight: "bold", textAlign: "center" }}
+          >
+            <p>
+              {data.educationForm.From} - {data.educationForm.To}
+            </p>
+          </div>
+          <div style={{ width: "50%", textAlign: "center" }}>
+            <p style={{ fontWeight: "bold" }}>
+              {data.educationForm.University}
+            </p>
+            <p>
+              {data.educationForm.City}
+            </p>
+            <p>
+            {data.educationForm.Degree}
+            </p>
+          </div>
+        </div>
+
+        {/* <p>{data.educationForm.University}</p>
         <p>{data.educationForm.City}</p>
-      </section>
-    );
-  };
+        <p>{data.educationForm.Degree}</p>
+        <p>{data.educationForm.From}</p>
+        <p>{data.educationForm.To}</p> */}
+      </div>
+    </section>
+  );
+};
 
 export default RenderEducation;
